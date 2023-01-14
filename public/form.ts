f443854d5a9e0703537a51c49291f0b7dd3fa8aa
ElvisGT <elvisgt99>
2023-01-14 16:56:38 -0500

    Trabajando en el formulario de login y recibir datos del backend

---
+++ public/form.ts
@@ -0,0 +1,39 @@
+
+function handleSubmit(event:FormDataEvent){
+  event.preventDefault()
+  const name:HTMLInputElement = document.querySelector("#name")! 
+  const password:HTMLInputElement = document.querySelector("#password")! 
+
+  registerUser(name.value,String(password.value))
+} 
+
+
+function registerUser(name:string,password:string){
+  const api = 'http://localhost:8080/api/v1/users'
+  const data = {
+   name,
+   password
+  }
+  fetch(api,{
+    method:"Post",
+    headers:{
+      'Content-Type':'application/json'
+    },
+    //@ts-ignore
+    body:JSON.stringify(data)
+  })
+  .then(result => result.json())
+  .then(data => {
+    const token = data.token
+    const url = 'http://localhost:8080/api/v1/contacts'
+    saveLocal(token)
+    window.location.replace(url)
+  })
+
+}
+
+function saveLocal(token:string){
+  if(!localStorage.getItem("token")){
+    localStorage.setItem("token",token)
+  }
+}
--- public/index.js
+++ public/index.js
@@ -1,1 +1,14 @@
-console.log("Elvis")
+let egt = document.getElementById('egt')
+const api = 'http://localhost:8080/api/v1/contacts'
+
+fetch(api,{
+  method:'GET',
+  headers:{
+    'x-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjczNTQ3MjY1fQ.iqRGIYgnFlelaQBH-RBnoXeOWBWlhK2nOuhImr9_eV8'
+  }
+}).then(result => result.json()).then(data => {
+  data.contacts.map(item => (
+    egt.innerHTML += `<li>${item.phonenumber}</li>`
+  ))
+  })
+
