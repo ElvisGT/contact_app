45aa1a609ac9928fc435d22d3097a507f31dfbe9
ElvisGT <elvisgiltorres@gmail.com>
2023-01-23 17:30:54 +0100

    Aplicacion lista para produccion

--- public/contacts.ts
+++ public/contacts.ts
@@ -1,9 +1,13 @@
 let apiContacts = `${window.location.origin}/api/v1/contacts`
 const title = document.querySelector(".contacts-title")
-const card = document.querySelector(".contacts-card")
+const card = document.querySelector(".contacts-card-container")
 const token = localStorage.getItem("token")
 const container = document.querySelector(".contacts-container")!
-const formContact: HTMLElement = document.getElementById("form-contact")!
+const formContact: HTMLElement = document.getElementById("contacts-form-container")!
+const addNew_btn: HTMLElement = document.getElementById("addNew-btn")!
+const logOut_btn: HTMLElement = document.getElementById("logOut-btn")!
+const back_btn: HTMLElement = document.getElementById("back-btn")!
+back_btn.style.display = 'none'
 
 
 if (!token) {
@@ -22,10 +26,12 @@ fetch(apiContacts, {
     title!.textContent += ' ' + data.total
     data.contacts.forEach(({ name, phonenumber, id }: any) => {
       card!.innerHTML += `
-      <h2>${name}</h2>
-      <p>${phonenumber}</p>
-      <button onclick="handleUpdate(${id})">Edit</button>
-      <button onclick="handleDelete(${id})">Delete</button>
+      <div class="contacts-card">
+        <h2>${name}</h2>
+        <h3>${phonenumber}</h3>
+        <button onclick="handleUpdate(${id})">Edit</button>
+        <button onclick="handleDelete(${id})">Delete</button>
+      </div>
     `
     });
   })
@@ -49,20 +55,24 @@ function handleUpdate(id: number) {
     .then(data => {
       formContact.style.display = 'none'
       container.innerHTML = `
-    <form onsubmit="submitContact('Put',${id},event)">
-      <label for="nameUpd">
-        Name
-        <input type="text" name="nameUpd" id="nameUpd" value="${data.name}"/>
-      </label>
-      
-      <label for="phonenumberUpd">
-        Phone Number
-        <input type="text" name="phonenumberUpd" id="phonenumberUpd" value="${data.phonenumber}"/>
-      </label>
+      <div class="contacts-update-container">
+          <form class="contact-form" onsubmit="submitContact('Put',${id},event)">
+            <label for="nameUpd">
+              <input class="nameUpd" type="text" name="nameUpd" id="nameUpd" value="${data.name}"/>
+            </label>
+            
+            <label for="phonenumberUpd">
+              <input class="phonenumberUpd" type="number" name="phonenumberUpd" id="phonenumberUpd" value="${data.phonenumber}"/>
+            </label>
 
-      <button type="submit">Enter</button>
-    </form>
-   `
+            <button type="submit">Enter</button>
+          </form>
+      </div>
+      `
+
+      addNew_btn.style.display = 'none'
+      logOut_btn.style.display = 'none'
+      back_btn.style.display = ''
     })
 
 }
@@ -84,18 +94,19 @@ function handleDelete(id: number) {
 
 function submitContact(useMethod: string, id?: number, event?: FormDataEvent) {
   event?.preventDefault()
+  
+  const getName: HTMLInputElement = document.querySelector(".contact-name")!
+  const getPhonenumber: HTMLInputElement = document.querySelector(".contact-phonenumber")!
+  let name = getName?.value
+  let phonenumber = Number(getPhonenumber?.value)
 
-  const getName: HTMLInputElement = document.querySelector("#name")!
-  const getPhonenumber: HTMLInputElement = document.querySelector("#phonenumber")!
-  let name = getName.value
-  let phonenumber = Number(getPhonenumber.value)
-
+  
   if (useMethod === 'Put') {
-    const getNameUpd: HTMLInputElement = document.querySelector("#nameUpd")!
-    const getPhonenumberUpd: HTMLInputElement = document.querySelector("#phonenumberUpd")!
+    const getNameUpd: HTMLInputElement = document.querySelector(".nameUpd")!
+    const getPhonenumberUpd: HTMLInputElement = document.querySelector(".phonenumberUpd")!
     apiContacts = `${apiContacts}/${id}`
-    name = getNameUpd.value
-    phonenumber = Number(getPhonenumberUpd.value)
+    name = getNameUpd?.value
+    phonenumber = Number(getPhonenumberUpd?.value)
   }
 
   const data = {
@@ -125,4 +136,25 @@ function logOut() {
   const url = `${window.location.origin}/users`
   localStorage.clear()
   window.location.replace(url)
+}
+
+function addNew(){
+  const formContainer: HTMLElement = document.getElementById("contacts-form-container")!
+  formContainer.innerHTML =  `
+    <form class="contact-form" id="form-contact" onsubmit="submitContact('Post',event)">
+    <label for="name">
+      <input class="contact-name" type="text" name="name" id="name" placeholder="Name"/>
+    </label>
+    
+    <label for="phonenumber">
+      <input class= "contact-phonenumber" type="number" name="phonenumber" id="phonenumber" placeholder="Phone Number" />
+    </label>
+
+    <button type="submit">Enter</button>
+    </form>
+  `
+}
+
+function backBtn(){
+  window.location.reload()
 }
